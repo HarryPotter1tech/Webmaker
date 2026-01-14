ed0b2c9d8416501f02782411b6295584444f3688
HarryPotter1tech <huangziang_2024@qq.com>
2026-01-14 11:08:26 +0800

    支持rag

--- backend/pdf_document_process.py
+++ backend/pdf_document_process.py
@@ -1,22 +1,24 @@
+from re import A
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
 from langchain_text_splitters import CharacterTextSplitter
 from docx import Document
 from sentence_transformers import SentenceTransformer
+import torch
 
 KNOWLEDGEBASE_PATH = r"E:\Research Training\knowledgebase"
 
 
-def retrive_init():
+async def documents_init(KNOWLEDGEBASE_PATH) -> list:
     PDFLoader = DirectoryLoader(
         KNOWLEDGEBASE_PATH, glob="**/*.pdf", loader_cls=PyPDFLoader
     )
     PDF_documents = PDFLoader.load()
-    print(f"loading pdf documents from{KNOWLEDGEBASE_PATH} now...")
+    print(f"loading pdf documents from {KNOWLEDGEBASE_PATH} now...")
     print(f"loaded {len(PDF_documents)} documents successfully.")
     return PDF_documents
 
 
-def retrive_split(documents) -> Document:
+async def documents_split(documents) -> Document:
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=400,
@@ -32,14 +34,20 @@ def retrive_split(documents) -> Document:
     return PDF_document_splited
 
 
-def documents_vectorize(document_splited) -> list:
+async def documents_vectorize(document_splited) -> list:
+    device = "cpu"
+    if torch.cuda.is_available():
+        print("CUDA is available. Using GPU for computations.")
+        device = "cuda"
+    else:
+        print("CUDA is not available. Using CPU for computations.")
     embedding_model = SentenceTransformer(
         r"E:\Research Training\embedding_model\jina-embeddings-v2-base-zh\jina-embeddings-v2-base-zh",
         trust_remote_code=True,
-        device="cpu",
+        device=device,
     )
     print(f"start to vectorize {len(document_splited)} pieces of documents now...")
-    embeddings = embedding_model.encode(
+    embeddings_vectors = embedding_model.encode(
         document_splited,
         batch_size=10,
         show_progress_bar=True,
@@ -47,14 +55,19 @@ def documents_vectorize(document_splited) -> list:
         convert_to_tensor=False,
     )
     print("documents vectorize successfully.")
-    for i in range(len(embeddings)):
-        print(f"{embeddings[i]}")
-    return embeddings
+    return embeddings_vectors
+
+
+async def vectors_retrive(vectors, vector_query: str):
+    retriver = vectors.as_retriever(search_type="similarity", search_kwargs={"k": 3})
+    retrive_results = retriver.get_relevant_documents(vector_query)
+    return retrive_results
 
 
 # 后续要考虑实现流式输出
 if __name__ == "__main__":
-    pdf_documents = retrive_init()
-    pdf_document_splited = retrive_split(pdf_documents)
+    pdf_documents = documents_init(KNOWLEDGEBASE_PATH)
+    pdf_document_splited = documents_split(pdf_documents)
     print(pdf_document_splited[124])
     pdf_documents_embeddings = documents_vectorize(pdf_document_splited)
+    print(pdf_documents_embeddings[124])
